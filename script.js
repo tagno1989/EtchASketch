ba28bda40bdf47091e45928dc768b9b44a90152e
tango1989 <jwarren2000@verizon.net>
2022-11-15 11:54:05 -0800

    added rainbow and opacity in script

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-// Divs for grid style layout for future color and drawings
 const sizeOfGrid = 16;
-const resetButton = document.querySelector('button')
+const reset = document.querySelector('button')
 const container = document.querySelector('.container')
+const createRandomRgb = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return {r , g , b}
+}
+
+
+// Divs for grid style layout for future color and drawings
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
     for (let i = 0; i < amtOfGrids; i++){
         const row = document.createElement('div')
         row.classList.add('grid-row')
 
         for (let j = 0; j < amtOfGrids; j++){
+            const {r , g , b} = createRandomRgb()
             const widthAndHeight = 960 / sizeOfGrid
             const gridBox = document.createElement('div')
             gridBox.classList.add('grid-box')
             gridBox.style.width = `${widthAndHeight}px`
             gridBox.style.height = `${widthAndHeight}px`
-            row.appendChild(gridBox)
-
+            
+// adding event listener to change the color in our div boxes // and opacity effect
             gridBox.addEventListener('mouseover' , () => {
-                gridBox.style.backgroundColor = 'black'
+                const bgColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+                // gridBox.style.backgroundColor = bgColor
+                const currentOpacity  = Number( gridBox.style.opacity)
+                gridBox.style.backgroundColor = bgColor
+                gridBox.style.opacity =Number(currentOpacity) + .3
             })
+            row.appendChild(gridBox)
         }
-
-
-        container.appendChild(row)
+        wrapper.appendChild(row)
     }
+    container.appendChild(wrapper)
 }
 
-createGrid(sizeOfGrid)
+
+
+reset.addEventListener('click', () => {
+    let userSize = Number(prompt('how big for new grid (must be less than 100)'))
+    while (userSize > 100) {
+        userSize = Number( prompt('how big for new grid (remember 100 or less)'))
+    }
+    const wrapper = document.querySelector('.wrapper')
+
+    if (!wrapper) {
+        createGrid(userSize)
+    } else {
+        wrapper.remove()
+        createGrid(userSize)
+    }
+    
+})
+
